45970a5a77a744f0001e340e65cf318def2f1a69
Ernestas Kulik <ernestas@nordsec.com>
2023-10-24 12:15:28 +0300

    Add test fixtures

---
+++ fixtures/src/lib.rs
@@ -0,0 +1,10 @@
+mod uniffi_fixtures {
+    arithmetical::uniffi_reexport_scaffolding!();
+    uniffi_callbacks::uniffi_reexport_scaffolding!();
+    custom_types::uniffi_reexport_scaffolding!();
+    uniffi_geometry::uniffi_reexport_scaffolding!();
+    uniffi_rondpoint::uniffi_reexport_scaffolding!();
+    uniffi_sprites::uniffi_reexport_scaffolding!();
+    uniffi_todolist::uniffi_reexport_scaffolding!();
+    uniffi_traits::uniffi_reexport_scaffolding!();
+}
