45e032db28128ca5f6dd0b1aa6738dcb3a33b0c3
Martynas Gurskas <gurmart@gmail.com>
2023-10-26 17:10:55 +0300

    Implement binding tests
    Signed-off-by: Martynas Gurskas <gurmart@gmail.com>

---
+++ cpp-tests/tests/arithmetic/main.cpp
@@ -0,0 +1,25 @@
+#include <test_common.hpp>
+
+#include <arithmetic.hpp>
+
+int main() {
+    ASSERT_EQ(arithmetic::add(2, 4), 6ul);
+    ASSERT_EQ(arithmetic::add(4, 8), 12ul);
+
+    ASSERT_EQ(2ul, arithmetic::sub(4, 2));
+    ASSERT_EQ(4ul, arithmetic::sub(8, 4));
+
+    // EXPECT_EXCEPTION(arithmetic::sub(2, 4), arithmetic::IntegerOverflow);
+
+    ASSERT_EQ(4ul, arithmetic::div(8, 2));
+
+    EXPECT_EXCEPTION(arithmetic::div(8, 0), std::runtime_error);
+
+    ASSERT_TRUE(arithmetic::equal(2, 2));
+    ASSERT_TRUE(arithmetic::equal(4, 4));
+
+    ASSERT_FALSE(arithmetic::equal(2, 4));
+    ASSERT_FALSE(arithmetic::equal(4, 8));
+
+    return 0;
+}
---
+++ cpp-tests/tests/include/test_common.hpp
@@ -0,0 +1,16 @@
+#include <cassert>
+#include <iostream>
+
+#define ASSERT_EQ(a, b) assert((a) == (b))
+#define ASSERT_NE(a, b) assert((a) != (b))
+#define ASSERT_TRUE(a) assert((a))
+#define ASSERT_FALSE(a) assert(!(a))
+
+#define EXPECT_EXCEPTION(expr, exception) \
+    try { \
+        expr; \
+        assert(false && "Didn't throw an exception"); \
+    } catch (exception const &) { \
+    } catch (...) { \
+        assert(false && "Exception didn't match" #exception); \
+    }
