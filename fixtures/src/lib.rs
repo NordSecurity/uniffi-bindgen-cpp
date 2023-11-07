f2e79a31613f56b3c63c2697715a11cd075d3b55
Martynas Gurskas <gurmart@gmail.com>
2023-11-07 10:18:35 +0200

    Add missing chronologic test fixtures
    Signed-off-by: Martynas Gurskas <gurmart@gmail.com>

--- fixtures/src/lib.rs
+++ fixtures/src/lib.rs
@@ -7,4 +7,5 @@ mod uniffi_fixtures {
     uniffi_sprites::uniffi_reexport_scaffolding!();
     uniffi_todolist::uniffi_reexport_scaffolding!();
     uniffi_traits::uniffi_reexport_scaffolding!();
+    uniffi_chronological::uniffi_reexport_scaffolding!();
 }
