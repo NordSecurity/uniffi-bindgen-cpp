85c8c0d821ae96480a154952a3cec70a7b63a681
Martynas Gurskas <martynas.gurskas@nordsec.com>
2023-11-13 11:15:10 +0200

    Fix void callbacks missing return type
    Signed-off-by: Martynas Gurskas <martynas.gurskas@nordsec.com>

--- bindgen/src/bindings/cpp/templates/callback.hpp
+++ bindgen/src/bindings/cpp/templates/callback.hpp
@@ -7,7 +7,7 @@ struct {{ class_name }} {
     {% for method in iface.methods() -%}
     virtual
     {%- match method.return_type() -%}
-    {% when Some with (return_type) %} {{ return_type|type_name }} {% else %} {% endmatch -%}
+    {% when Some with (return_type) %} {{ return_type|type_name }} {% else %} void {% endmatch -%}
     {{ method.name()|fn_name }}({% call macros::param_list_with_default(method) %}) = 0;
     {%- endfor %}
 };
