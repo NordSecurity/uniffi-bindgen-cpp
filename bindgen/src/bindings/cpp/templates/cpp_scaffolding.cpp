d06719c4ac85b97902af5dc28e71bc501c13bba3
Martynas Gurskas <martynas.gurskas@nordsec.com>
2024-01-26 11:31:02 +0200

    Add support for callbacks in cpp scaffolding
    Signed-off-by: Martynas Gurskas <martynas.gurskas@nordsec.com>

--- bindgen/src/bindings/cpp/templates/cpp_scaffolding.cpp
+++ bindgen/src/bindings/cpp/templates/cpp_scaffolding.cpp
@@ -1,3 +1,4 @@
+{%- import "macros.cpp" as macros %}
 {% let namespace = ci.namespace() %}
 {% match config.namespace %}
 {% when Some with (ns) %}
@@ -12,6 +13,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <memory>
+#include <atomic>
 
 using namespace {{ namespace }};
 
@@ -30,6 +32,8 @@ struct RustCallStatus {
     int8_t code;
     RustBuffer error_buf;
 };
+
+typedef int ForeignCallback(uint64_t handle, uint32_t method, uint8_t *args_data, int32_t args_len, RustBuffer *buf_ptr);
 
 struct RustStreamBuffer: std::basic_streambuf<uint8_t> {
     RustStreamBuffer(RustBuffer *buf) {
@@ -116,6 +120,21 @@ private:
 {% include "rec_conv.hpp" %}
 {%- when Type::Optional { inner_type } %}
 {% include "opt_conv.hpp" %}
+{%- when Type::CallbackInterface { module_path, name } %}
+{%- let ffi_converter_name = typ|ffi_converter_name %}
+{%- let type_name = typ|type_name %}
+{%- let class_name = type_name|class_name %}
+{%- let canonical_type_name = typ|canonical_name %}
+struct {{ ffi_converter_name|class_name }} {
+    static {{ type_name }} lift(uint64_t);
+    static uint64_t lower(const {{ type_name }} &);
+    static {{ type_name }} read(RustStream &);
+    static void write(RustStream &, const {{ type_name }} &);
+    static int32_t allocation_size(const {{ type_name }} &);
+
+
+    static std::atomic<uint64_t> fn_handle;
+};
 {% else %}
 {%- endmatch %}
 {%- endfor %}
@@ -165,7 +184,7 @@ RustBuffer {{ ci.ffi_rustbuffer_reserve().name() }}(RustBuffer buffer, int32_t a
         .data = (uint8_t*)std::realloc(buffer.data, buffer.capacity + additional),
     };
 
-    return std::move(buf); 
+    return std::move(buf);
 }
 
 {% for func in ci.function_definitions() %}
@@ -177,13 +196,32 @@ RustBuffer {{ ci.ffi_rustbuffer_reserve().name() }}(RustBuffer buffer, int32_t a
 {% endfor %}
 {%- if ffi_func.has_rust_call_status_arg() %}RustCallStatus *out_status{% endif -%}
 ) {
+    {%- if ffi_func.has_rust_call_status_arg() %}
+    out_status->code = 0;
+    {% endif -%}
+
+    {% match func.return_type() %}
+    {% when Some with (return_type) %}
     auto ret = {{ namespace }}::{{ func.name() }}(
     {%- for arg in func.arguments() %}
     {{- arg|lift_fn }}({{ arg.name()|var_name }}){% if !loop.last %}, {% endif -%}
     {% endfor %});
+    return {{ return_type|lower_fn }}(ret);
+    {% when None %}
+    {{ namespace }}::{{ func.name() }}(
+    {%- for arg in func.arguments() %}
+    {{- arg|lift_fn }}({{ arg.name()|var_name }}){% if !loop.last %}, {% endif -%}
+    {% endfor %});
+    {% endmatch %}
+}
+{% endfor %}
 
-    {% match func.return_type() %}
-    {% when Some with (return_type) %}return {{ return_type|lower_fn }}(ret);{% when None %}{% endmatch %}
+{% for func in ci.callback_interface_definitions() %}
+{% let ffi_func = func.ffi_init_callback() %}
+void {{ ffi_func.name() }}(ForeignCallback callback_stub, RustCallStatus *out_status) {
+    out_status->code = 0;
+
+    {{ func|ffi_converter_name }}::fn_handle.store(reinterpret_cast<uint64_t>(callback_stub));
 }
 {% endfor %}
 
@@ -194,7 +232,7 @@ RustBuffer {{ ci.ffi_rustbuffer_reserve().name() }}(RustBuffer buffer, int32_t a
 {{- arg.type_().borrow()|ffi_type_name }} {{ arg.name() }}{% if !loop.last || func.has_rust_call_status_arg() %}, {% endif -%}
 {% endfor %}
 {%- if func.has_rust_call_status_arg() %}RustCallStatus *out_status{% endif -%}
-) { 
+) {
     {%- match func.return_type() %}{% when Some with (return_type) %}
         {% match return_type %}
         {% when FfiType::RustArcPtr(_) %}
@@ -285,6 +323,89 @@ void rustbuffer_free(RustBuffer& buf) {
 {% include "rec.cpp" %}
 {%- when Type::Optional { inner_type } %}
 {% include "opt_tmpl.cpp" %}
+{%- when Type::CallbackInterface { module_path, name } %}
+{%- let ffi_converter_name = typ|ffi_converter_name %}
+{%- let type_name = typ|type_name %}
+{%- let class_name = type_name|class_name %}
+{%- let canonical_type_name = typ|canonical_name %}
+{%- let iface = ci|get_callback_interface_definition(name) %}
+{{ type_name }} {{ ffi_converter_name|class_name }}::lift(uint64_t handle) {
+    class {{ iface.name() }}Proxy: public {{ iface.name() }} {
+        public:
+            {{ iface.name() }}Proxy(uint64_t handle): handle(handle) { }
+
+            {% for m in iface.methods() %}
+            {%- match m.return_type() -%}
+            {% when Some with (return_type) %}{{ return_type|type_name }} {% when None %}void {% endmatch %}{{ m.name() }}(
+            {%- for arg in m.arguments() %}
+            {{- arg.as_type().borrow()|type_name }} {{ arg.name() }}{% if !loop.last %}, {% endif -%}
+            {% endfor %}) override {
+                ForeignCallback *callback_stub = reinterpret_cast<ForeignCallback *>({{ ffi_converter_name|class_name }}::fn_handle.load());
+                if (callback_stub == nullptr) {
+                    throw std::runtime_error("callback stub not initialized, this indicates a bug in the generated code");
+                }
+
+                RustBuffer out_buf = {
+                    .capacity = 0,
+                    .len = 0,
+                    .data = nullptr,
+                };
+
+                int32_t size = 0;
+                {% for arg in m.arguments() %}
+                size += {{ arg|allocation_size_fn }}({{ arg.name() }});
+                {% endfor %}
+                RustBuffer in_buf = rustbuffer_alloc(size);
+                RustStream in_stream(&in_buf);
+
+                {% for arg in m.arguments() %}
+                {{ arg|write_fn }}(in_stream, {{ arg.name() }});
+                {% endfor %}
+
+                auto ret = callback_stub(this->handle, {{ loop.index }}, in_buf.data, size, &out_buf);
+
+                rustbuffer_free(in_buf);
+
+                {% match m.return_type() %}
+                {% when Some with (return_type) %}
+                RustStream out_stream(&out_buf);
+                auto result = {{ return_type|read_fn }}(out_stream);
+                rustbuffer_free(out_buf);
+
+                return result;
+                {% else %}
+                rustbuffer_free(out_buf);
+                {% endmatch %}
+            }
+            {% endfor %}
+        private:
+            uint64_t handle;
+    };
+
+    return std::make_shared<{{ iface.name() }}Proxy>(handle);
+}
+
+uint64_t {{ ffi_converter_name|class_name }}::lower(const {{ type_name }} &obj) {
+    return {{ ffi_converter_name|class_name }}::fn_handle.load();
+}
+
+{{ type_name }} {{ ffi_converter_name|class_name }}::read(RustStream &stream) {
+    uint64_t handle;
+    stream >> handle;
+
+    return {{ ffi_converter_name|class_name }}::lift(handle);
+}
+
+void {{ ffi_converter_name|class_name }}::write(RustStream &stream, const {{ type_name }} &obj) {
+    stream << {{ ffi_converter_name|class_name }}::lower(obj);
+}
+
+int32_t {{ ffi_converter_name|class_name }}::allocation_size(const {{ type_name }} &obj) {
+    return sizeof(uint64_t);
+}
+
+std::atomic<uint64_t> {{ ffi_converter_name|class_name }}::fn_handle = 0;
+
 {%- else %}
 {%- endmatch %}
 {%- endfor %}
