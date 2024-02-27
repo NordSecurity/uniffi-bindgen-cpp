00f2257ae0abd305731be1cd297f9a2964a8caa6
Martynas Gurskas <martynas.gurskas@nordsec.com>
2024-02-27 10:05:34 +0200

    Keep an internal refcount in `ObjectMap` to properly handle entry removal
    Signed-off-by: Martynas Gurskas <martynas.gurskas@nordsec.com>

--- bindgen/src/bindings/cpp/templates/scaffolding/object_map.cpp
+++ bindgen/src/bindings/cpp/templates/scaffolding/object_map.cpp
@@ -4,24 +4,30 @@ template <typename T> struct ObjectMap {
     std::shared_ptr<T> at(uint64_t ptr) {
         std::lock_guard<std::mutex> guard(this->mutex);
 
-        return this->map.at(ptr);
+        return this->map.at(ptr).ptr;
     }
 
     uint64_t insert(std::shared_ptr<T> impl) {
         std::lock_guard<std::mutex> guard(this->mutex);
+        auto key = (uint64_t)impl.get();
 
-        this->map.insert({ (uint64_t)impl.get(), impl });
+        if (this->map.contains(key)) {
+            this->map.at(key).ref_count += 1;
+        } else {
+            this->map.insert({ key, {impl, 1} });
+        }
 
-        return (uint64_t)impl.get();
+        return key;
     }
 
     void erase(uint64_t ptr) {
         std::lock_guard<std::mutex> guard(this->mutex);
 
-        if (this->map.find(ptr) != this->map.end()) {
-            if (this->map.at(ptr).use_count() == 1) {
-                this->map.erase(ptr);
-            }
+        auto &wrapper = this->map.at(ptr);
+        wrapper.ref_count -= 1;
+
+        if (wrapper.ref_count == 0) {
+            this->map.erase(ptr);
         }
     }
     private:
@@ -31,6 +37,11 @@ template <typename T> struct ObjectMap {
         ObjectMap<T> &operator=(const ObjectMap<T> &) = delete;
         ObjectMap<T> &operator=(ObjectMap<T> &&) = delete;
 
+        struct PtrWrapper {
+            std::shared_ptr<T> ptr = nullptr;
+            uint64_t ref_count = 0;
+        };
+
         std::mutex mutex;
-        std::map<uint64_t, std::shared_ptr<T>> map;
+        std::map<uint64_t, PtrWrapper> map;
 };
