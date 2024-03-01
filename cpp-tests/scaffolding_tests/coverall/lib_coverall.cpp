7fae6b76a13128c5bd9c18400c1720e497761d37
Martynas Gurskas <martynas.gurskas@nordsec.com>
2024-03-01 12:36:00 +0200

    Add coverall scaffolding test
    Signed-off-by: Martynas Gurskas <martynas.gurskas@nordsec.com>

---
+++ cpp-tests/scaffolding_tests/coverall/lib_coverall.cpp
@@ -0,0 +1,285 @@
+#include "lib_coverall.hpp"
+#include <chrono>
+#include <cstdint>
+#include <iostream>
+#include <thread>
+
+std::atomic_uint64_t NUM_ALIVE = 0;
+
+coverall::Coveralls::Coveralls(std::string name, bool should_panic) {
+    if (!should_panic) {
+        NUM_ALIVE += 1;
+        this->name = name;
+    } else {
+        throw coverall_error::TooManyHoles();
+    }
+}
+
+coverall::Coveralls::Coveralls(std::string name) {
+    NUM_ALIVE += 1;
+    this->name = name;
+}
+
+coverall::Coveralls::~Coveralls() {
+    NUM_ALIVE -= 1;
+}
+
+bool coverall::IFirst::compare(std::shared_ptr<ISecond> other) {
+    return false;
+}
+
+bool coverall::ISecond::compare(std::shared_ptr<IFirst> other) {
+    return false;
+}
+
+void coverall::ThreadsafeCounter::busy_wait(int32_t ms) {
+    this->is_busy = true;
+    std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+    this->is_busy = false;
+}
+
+int32_t coverall::ThreadsafeCounter::increment_if_busy() {
+    if (this->is_busy) {
+        return this->count += 1;
+    } else {
+        return this->count;
+    }
+}
+
+coverall::Color coverall::Patch::get_color() {
+    return this->color;
+}
+
+std::string coverall::Coveralls::get_name() {
+    return this->name;
+}
+
+bool coverall::Coveralls::maybe_throw(bool should_throw) {
+    if (should_throw) {
+        throw coverall_error::TooManyHoles();
+    } else {
+        return true;
+    }
+}
+
+// Note: This should test Rust's Into, we don't really have this here
+bool coverall::Coveralls::maybe_throw_into(bool should_throw) {
+    if (should_throw) {
+        throw coverall_error::TooManyHoles();
+    } else {
+            return true;
+    }
+}
+
+bool coverall::Coveralls::maybe_throw_complex(uint8_t input) {
+    switch(input) {
+    case 0:
+        return true;
+    case 1: {
+        auto err = complex_error::OsError();
+        err.code = 10;
+        err.extended_code = 20;
+
+        throw err;
+    }
+    case 2: {
+        auto err = complex_error::PermissionDenied();
+        err.reason = "Forbidden";
+
+        throw err;
+    }
+    case 3:
+        throw complex_error::UnknownError();
+    default:
+        throw std::runtime_error("Invalid input");
+    }
+}
+
+void coverall::Coveralls::panic(std::string message) {
+    throw std::runtime_error("coveralls->panic()");
+}
+
+void coverall::Coveralls::fallible_panic(std::string message) {
+    throw std::runtime_error(message);
+}
+
+uint64_t coverall::Coveralls::strong_count(const std::shared_ptr<Coveralls> &self) {
+    return self.use_count();
+}
+
+void coverall::Coveralls::take_other(const std::shared_ptr<Coveralls> &other) {
+    std::lock_guard<std::mutex> lock(this->other_mutex);
+    this->other = other;
+}
+
+std::shared_ptr<coverall::Coveralls> coverall::Coveralls::get_other() {
+    std::lock_guard<std::mutex> lock(this->other_mutex);
+    return this->other;
+}
+
+void coverall::Coveralls::take_other_fallible() {
+    throw coverall::coverall_error::TooManyHoles();
+}
+
+void coverall::Coveralls::take_other_panic(std::string message) {
+    throw std::runtime_error(message);
+}
+
+std::shared_ptr<coverall::Coveralls> coverall::Coveralls::clone_me() {
+    std::lock_guard<std::mutex> lock(this->other_mutex);
+
+    auto clone = std::make_shared<Coveralls>(this->name);
+    clone->other = this->other;
+
+    return clone;
+}
+
+std::string coverall::Coveralls::get_status(std::string status) {
+    return "status: " + status;
+}
+
+std::unordered_map<std::string, uint64_t> coverall::Coveralls::get_dict(std::string key, uint64_t value) {
+    std::unordered_map<std::string, uint64_t> map;
+    map.insert({ key, value });
+
+    return map;
+}
+
+std::unordered_map<std::string, uint64_t> coverall::Coveralls::get_dict2(std::string key, uint64_t value) {
+    std::unordered_map<std::string, uint64_t> map;
+    map.insert({ key, value });
+
+    return map;
+}
+
+std::unordered_map<uint32_t, uint64_t> coverall::Coveralls::get_dict3(uint32_t key, uint64_t value) {
+    std::unordered_map<uint32_t, uint64_t> map;
+    map.insert({ key, value });
+
+    return map;
+}
+
+void coverall::Coveralls::add_patch(std::shared_ptr<Patch> patch) {
+    auto repair = Repair {
+        .when = std::chrono::high_resolution_clock::now(),
+        .patch = patch
+    };
+
+    std::lock_guard<std::mutex> lock(this->repairs_mutex);
+    this->repairs.push_back(repair);
+}
+
+void coverall::Coveralls::add_repair(Repair repair) {
+    std::lock_guard<std::mutex> lock(this->repairs_mutex);
+    this->repairs.push_back(repair);
+}
+
+std::vector<coverall::Repair> coverall::Coveralls::get_repairs() {
+    std::lock_guard<std::mutex> lock(this->repairs_mutex);
+    return this->repairs;
+}
+
+std::vector<uint8_t> coverall::Coveralls::reverse(std::vector<uint8_t> value) {
+    return std::vector<uint8_t>(value.rbegin(), value.rend());
+}
+
+
+coverall::SimpleDict coverall::create_some_dict() {
+    std::string some_bytes_string = "some_bytes";
+    std::string maybe_some_bytes_string = "maybe_some_bytes";
+
+    return {
+        .text = "text",
+        .maybe_text = "maybe_text",
+        .some_bytes = std::vector<uint8_t>(some_bytes_string.begin(), some_bytes_string.end()),
+        .maybe_some_bytes = std::vector<uint8_t>(maybe_some_bytes_string.begin(), maybe_some_bytes_string.end()),
+        .a_bool = true,
+        .maybe_a_bool = false,
+        .unsigned8 = 1,
+        .maybe_unsigned8 = 2,
+        .unsigned16 = 3,
+        .maybe_unsigned16 = 4,
+        .unsigned64 = std::numeric_limits<uint64_t>::max(),
+        .maybe_unsigned64 = std::numeric_limits<uint64_t>::min(),
+        .signed8 = 8,
+        .maybe_signed8 = 0,
+        .signed64 = std::numeric_limits<int64_t>::max(),
+        .maybe_signed64 = 0,
+        .float32 = 1.2345,
+        .maybe_float32 = 22.0 / 7.0,
+        .float64 = 0.0,
+        .maybe_float64 = 1.0,
+        .coveralls = std::make_shared<Coveralls>("some_dict"),
+        .test_trait = nullptr,
+    };
+}
+
+coverall::SimpleDict coverall::create_none_dict() {
+    return {};
+}
+
+uint64_t coverall::get_num_alive() {
+    return NUM_ALIVE;
+}
+
+std::vector<std::shared_ptr<coverall::TestTrait>> coverall::get_traits() {
+    return {};
+}
+
+coverall::MaybeSimpleDict coverall::get_maybe_simple_dict(int8_t index) {
+    if (index == 0) {
+        return { coverall::MaybeSimpleDict::YEAH {} };
+    }
+    else if (index == 1) {
+        return { coverall::MaybeSimpleDict::NAH {} };
+    }
+
+    throw std::runtime_error("invalid index");
+}
+
+coverall::SimpleFlatMacroEnum coverall::get_simple_flat_macro_enum(int8_t index) {
+    if (index == 0) {
+        return { coverall::SimpleFlatMacroEnum::FIRST { "the first" } };
+    }
+    else if (index == 1) {
+        return { coverall::SimpleFlatMacroEnum::SECOND { 2 } };
+    }
+
+    throw std::runtime_error("invalid index");
+}
+
+void coverall::println(std::string text) {
+    std::cout << text << std::endl;
+}
+
+void coverall::throw_macro_error() {
+    throw coverall::coverall_macro_error::TooManyMacros();
+}
+
+void coverall::throw_flat_error() {
+    auto error = coverall::coverall_flat_error::TooManyVariants();
+    error.num = 99;
+
+    throw error;
+}
+
+void coverall::throw_flat_macro_error() {
+    auto error = coverall::coverall_flat_macro_error::TooManyVariants();
+    error.num = 88;
+
+    throw error;
+}
+
+void coverall::throw_rich_error_no_variant_data() {
+    throw coverall::coverall_rich_error_no_variant_data::TooManyPlainVariants();
+}
+
+void coverall::throw_complex_macro_error() {
+    auto error = coverall::complex_macro_error::OsError();
+    error.code = 1;
+    error.extended_code = 2;
+
+    throw error;
+}
+
+#include <coverall_cpp_scaffolding.cpp>
---
+++ cpp-tests/scaffolding_tests/coverall/lib_coverall.hpp
@@ -0,0 +1,319 @@
+#include <cstdint>
+#include <stdexcept>
+#include <string>
+#include <optional>
+#include <vector>
+#include <variant>
+#include <unordered_map>
+#include <chrono>
+#include <memory>
+#include <mutex>
+#include <atomic>
+
+namespace {
+    namespace coverall {
+        typedef std::chrono::time_point<std::chrono::system_clock> timestamp;
+
+        class IFirst {
+        public:
+            IFirst() = default;
+
+            bool compare(std::shared_ptr<class ISecond> other);
+        };
+
+        class ISecond {
+        public:
+            ISecond() = default;
+
+            bool compare(std::shared_ptr<IFirst> other);
+        };
+
+        class ThreadsafeCounter {
+        public:
+            ThreadsafeCounter() = default;
+
+            void busy_wait(int32_t ms);
+            int32_t increment_if_busy();
+        private:
+            std::atomic_bool is_busy;
+            std::atomic_int32_t count;
+        };
+
+        class TestTrait {
+        public:
+            virtual std::string name() = 0;
+
+            virtual uint64_t number(std::shared_ptr<TestTrait> self) = 0;
+            virtual uint64_t strong_count(std::shared_ptr<TestTrait> self) = 0;
+
+            virtual void take_other(std::shared_ptr<TestTrait> other) = 0;
+            virtual std::shared_ptr<TestTrait> get_other() = 0;
+        };
+
+        enum class Color {
+            RED,
+            BLUE,
+            GREEN
+        };
+
+        class Patch {
+        public:
+            Patch(Color color) : color(color) {}
+
+            Color get_color();
+        private:
+            Color color;
+        };
+
+        struct Repair {
+            timestamp when;
+            std::shared_ptr<Patch> patch;
+        };
+
+        class Coveralls {
+        public:
+            Coveralls(std::string name, bool should_fail);
+            Coveralls(std::string name);
+
+            ~Coveralls();
+
+            std::string get_name();
+
+            bool maybe_throw(bool should_throw);
+            bool maybe_throw_into(bool should_throw);
+            bool maybe_throw_complex(uint8_t input);
+
+            void panic(std::string message);
+
+            void fallible_panic(std::string message);
+
+            uint64_t strong_count(const std::shared_ptr<Coveralls> &self);
+
+            void take_other(const std::shared_ptr<Coveralls> &other);
+            std::shared_ptr<Coveralls> get_other();
+            void take_other_fallible();
+            void take_other_panic(std::string message);
+
+            std::shared_ptr<Coveralls> clone_me();
+
+            std::string get_status(std::string status);
+
+            std::unordered_map<std::string, uint64_t> get_dict(std::string key, uint64_t value);
+            std::unordered_map<std::string, uint64_t> get_dict2(std::string key, uint64_t value);
+            std::unordered_map<uint32_t, uint64_t> get_dict3(uint32_t key, uint64_t value);
+
+            void add_patch(std::shared_ptr<Patch> patch);
+            void add_repair(Repair repair);
+            std::vector<Repair> get_repairs();
+
+            std::vector<uint8_t> reverse(std::vector<uint8_t> value);
+        private:
+            std::string name;
+
+            std::shared_ptr<Coveralls> other;
+            std::mutex other_mutex;
+
+            std::vector<Repair> repairs;
+            std::mutex repairs_mutex;
+        };
+
+        struct SimpleDict {
+            std::string text;
+            std::optional<std::string> maybe_text;
+            std::vector<uint8_t> some_bytes;
+            std::optional<std::vector<uint8_t>> maybe_some_bytes;
+            bool a_bool;
+            std::optional<bool> maybe_a_bool;
+            uint8_t unsigned8;
+            std::optional<uint8_t> maybe_unsigned8;
+            uint16_t unsigned16;
+            std::optional<uint16_t> maybe_unsigned16;
+            uint64_t unsigned64;
+            std::optional<uint64_t> maybe_unsigned64;
+            int8_t signed8;
+            std::optional<int8_t> maybe_signed8;
+            int64_t signed64;
+            std::optional<int64_t> maybe_signed64;
+            float float32;
+            std::optional<float> maybe_float32;
+            double float64;
+            std::optional<double> maybe_float64;
+            std::shared_ptr<Coveralls> coveralls;
+            std::shared_ptr<TestTrait> test_trait;
+        };
+
+        struct DictWithDefaults {
+            std::string name;
+            std::optional<std::string> category;
+            uint64_t integer;
+        };
+
+        class MaybeSimpleDict {
+        public:
+            struct YEAH {
+                SimpleDict d;
+            };
+
+            struct NAH {};
+
+            MaybeSimpleDict(MaybeSimpleDict::YEAH value) { this->variant = value; }
+            MaybeSimpleDict(MaybeSimpleDict::NAH value) { this->variant = value; }
+
+            std::variant<YEAH, NAH> variant;
+        };
+
+        class SimpleFlatEnum {
+        public:
+            struct FIRST {
+                std::string val;
+            };
+
+            struct SECOND {
+                uint16_t num;
+            };
+
+            SimpleFlatEnum(SimpleFlatEnum::FIRST value) { this->variant = value; }
+            SimpleFlatEnum(SimpleFlatEnum::SECOND value) { this->variant = value; }
+
+            std::variant<FIRST, SECOND> variant;
+        };
+
+        class SimpleFlatMacroEnum {
+        public:
+            struct FIRST {
+                std::string val;
+            };
+
+            struct SECOND {
+                uint16_t num;
+            };
+
+            SimpleFlatMacroEnum(SimpleFlatMacroEnum::FIRST value) { this->variant = value; }
+            SimpleFlatMacroEnum(SimpleFlatMacroEnum::SECOND value) { this->variant = value; }
+
+            std::variant<FIRST, SECOND> variant;
+        };
+
+        namespace coverall_error {
+            class TooManyHoles: public std::runtime_error {
+            public:
+                TooManyHoles() : TooManyHoles("") {}
+                TooManyHoles(const std::string &what_arg) : std::runtime_error(what_arg) {}
+            };
+        }
+
+        namespace coverall_macro_error {
+            class TooManyHoles: public std::runtime_error {
+            public:
+                TooManyHoles() : TooManyHoles("") {}
+                TooManyHoles(const std::string &what_arg) : std::runtime_error(what_arg) {}
+            };
+
+            class TooManyMacros: public std::runtime_error {
+            public:
+                TooManyMacros() : TooManyMacros("") {}
+                TooManyMacros(const std::string &what_arg) : std::runtime_error(what_arg) {}
+            };
+        }
+
+        namespace coverall_flat_error {
+            class TooManyVariants: public std::runtime_error {
+                public:
+                    TooManyVariants() : TooManyVariants("") {}
+                    TooManyVariants(const std::string &what_arg) : std::runtime_error(what_arg) {}
+
+                    int16_t num;
+            };
+        }
+
+        namespace coverall_flat_macro_error {
+            class TooManyVariants: public std::runtime_error {
+                public:
+                    TooManyVariants() : TooManyVariants("") {}
+                    TooManyVariants(const std::string &what_arg) : std::runtime_error(what_arg) {}
+
+                    int16_t num;
+            };
+        }
+
+
+        namespace coverall_rich_error_no_variant_data {
+            class TooManyPlainVariants: public std::runtime_error {
+                public:
+                    TooManyPlainVariants() : TooManyPlainVariants("") {}
+                    TooManyPlainVariants(const std::string &what_arg) : std::runtime_error(what_arg) {}
+            };
+        }
+
+        namespace complex_error {
+            class OsError: public std::runtime_error {
+                public:
+                    OsError() : OsError("") {}
+                    OsError(const std::string &what_arg) : std::runtime_error(what_arg) {}
+
+                    int16_t code;
+                    int16_t extended_code;
+            };
+
+            class PermissionDenied: public std::runtime_error {
+                public:
+                    PermissionDenied() : PermissionDenied("") {}
+                    PermissionDenied(const std::string &what_arg) : std::runtime_error(what_arg) {}
+
+                    std::string reason;
+            };
+
+            class UnknownError: public std::runtime_error {
+                public:
+                    UnknownError() : UnknownError("") {}
+                    UnknownError(const std::string &what_arg) : std::runtime_error(what_arg) {}
+            };
+        }
+
+        namespace complex_macro_error {
+            class OsError: public std::runtime_error {
+                public:
+                    OsError() : OsError("") {}
+                    OsError(const std::string &what_arg) : std::runtime_error(what_arg) {}
+
+                    int16_t code;
+                    int16_t extended_code;
+            };
+
+            class PermissionDenied: public std::runtime_error {
+                public:
+                    PermissionDenied() : PermissionDenied("") {}
+                    PermissionDenied(const std::string &what_arg) : std::runtime_error(what_arg) {}
+
+                    std::string reason;
+            };
+
+            class UnknownError: public std::runtime_error {
+                public:
+                    UnknownError() : UnknownError("") {}
+                    UnknownError(const std::string &what_arg) : std::runtime_error(what_arg) {}
+            };
+        }
+
+        SimpleDict create_some_dict();
+        SimpleDict create_none_dict();
+
+        uint64_t get_num_alive();
+
+        std::vector<std::shared_ptr<TestTrait>> get_traits();
+
+        MaybeSimpleDict get_maybe_simple_dict(int8_t index);
+
+        SimpleFlatMacroEnum get_simple_flat_macro_enum(int8_t index);
+
+        void println(std::string text);
+
+        void throw_macro_error();
+
+        void throw_flat_error();
+        void throw_flat_macro_error();
+
+        void throw_rich_error_no_variant_data();
+        void throw_complex_macro_error();
+    }
+}
