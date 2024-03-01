d63ea238fa6ef2c273e22ab8b2e59ff057dae9f1
Martynas Gurskas <martynas.gurskas@nordsec.com>
2024-03-01 16:35:28 +0200

    Move `RustStream` implementation to a separate template file
    Signed-off-by: Martynas Gurskas <martynas.gurskas@nordsec.com>

--- bindgen/src/bindings/cpp/templates/wrapper.hpp
+++ bindgen/src/bindings/cpp/templates/wrapper.hpp
@@ -79,54 +79,7 @@ typedef {{ type_name }} {{ name }};
 {%- endfor %}
 
 namespace uniffi {
-struct RustStreamBuffer: std::basic_streambuf<uint8_t> {
-    RustStreamBuffer(RustBuffer *buf) {
-        this->setg(buf->data, buf->data, buf->data + buf->len);
-        this->setp(buf->data, buf->data + buf->capacity);
-    }
-    ~RustStreamBuffer() = default;
-
-private:
-    RustStreamBuffer() = delete;
-    RustStreamBuffer(const RustStreamBuffer &) = delete;
-    RustStreamBuffer(RustStreamBuffer &&) = delete;
-
-    RustStreamBuffer &operator=(const RustStreamBuffer &) = delete;
-    RustStreamBuffer &operator=(RustStreamBuffer &&) = delete;
-};
-
-struct RustStream: std::basic_iostream<uint8_t> {
-    RustStream(RustBuffer *buf):
-        streambuf(RustStreamBuffer(buf)), std::basic_iostream<uint8_t>(&streambuf) { }
-
-    template <typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
-    RustStream &operator>>(T &val) {
-        read(reinterpret_cast<uint8_t *>(&val), sizeof(T));
-
-        if (std::endian::native != std::endian::big) {
-            auto bytes = reinterpret_cast<char *>(&val);
-
-            std::reverse(bytes, bytes + sizeof(T));
-        }
-
-        return *this;
-    }
-
-    template <typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
-    RustStream &operator<<(T val) {
-        if (std::endian::native != std::endian::big) {
-            auto bytes = reinterpret_cast<char *>(&val);
-
-            std::reverse(bytes, bytes + sizeof(T));
-        }
-
-        write(reinterpret_cast<uint8_t *>(&val), sizeof(T));
-
-        return *this;
-    }
-private:
-    RustStreamBuffer streambuf;
-};
+{%- include "rust_buf_stream.cpp" %}
 
 RustBuffer rustbuffer_alloc(int32_t);
 RustBuffer rustbuffer_from_bytes(const ForeignBytes &);
