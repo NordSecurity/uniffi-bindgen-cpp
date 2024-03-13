fc7011ea577076cfb75292a87d2feaf5417a927f
Martynas Gurskas <martynas.gurskas@nordsec.com>
2024-03-13 13:34:58 +0200

    Use an exclude list when selecting Go tests instead of specifying which tests to run
    Signed-off-by: Martynas Gurskas <martynas.gurskas@nordsec.com>

--- test_scaffolding_go.sh
+++ test_scaffolding_go.sh
@@ -20,22 +20,31 @@ cargo build
 ./build_bindings.sh
 
 
+pushd $BINDINGS_DIR
+
+# We exclude tests that are not part of the main Rust fixtures
+EXCLUDE_LIST="custom_types_test\
+|destroy_test\
+|docstring_test\
+|errors_test\
+|ext_types_test\
+|foreign_executor_test\
+|futures_test\
+|name_case_test\
+|objects_test\
+|proc_macro_test\
+|simple_fns_test\
+|simple_iface_test\
+|trait_methods_test\
+|type_limits_test"
+
+FILTERED_LIST=$(ls -1 *.go | grep -E -v "$EXCLUDE_LIST")
+
 # We add the skip section, that skips a few chronological test functions
 # due to C++ chrono library not supporting large timestamp values
-pushd $BINDINGS_DIR
 LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-}:$BINARIES_DIR" \
 	CGO_LDFLAGS="-luniffi_fixtures -L$BINARIES_DIR -lm -ldl" \
 	CGO_ENABLED=1 \
-	go test \
-	binding_tests.go \
-	arithmetic_test.go \
-	callbacks_test.go \
-	fixture_callbacks_test.go \
-	geometry_test.go \
-	rondpoint_test.go \
-	sprites_test.go \
-	todolist_test.go \
-	coverall_test.go \
-	chronological_test.go \
+	go test $FILTERED_LIST \
 	-skip "TestTimestampMinMax|TestPreEpochTimestampsSerializesCorrectly" \
 	-v
