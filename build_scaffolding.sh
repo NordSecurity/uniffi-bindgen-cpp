cd8909da0c3a090e327fc3ac3d54cdc493925e14
Martynas Gurskas <martynas.gurskas@nordsec.com>
2024-03-13 15:48:59 +0200

    Split build and test steps into separate jobs
    so we can kick off the external generator tests faster
    
    Signed-off-by: Martynas Gurskas <martynas.gurskas@nordsec.com>

--- build_scaffolding.sh
+++ build_scaffolding.sh
@@ -4,4 +4,4 @@ set -euxo pipefail
 mkdir -p cpp-tests/build
 cd cpp-tests/build
 cmake -DCMAKE_BUILD_TYPE=Debug ..
-make
+make uniffi_fixtures
--- test_bindings.sh
+++ test_bindings.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -euxo pipefail
 
+mkdir -p cpp-tests/build
 cd cpp-tests/build
+cmake -DCMAKE_BUILD_TYPE=Debug ..
+make
 CTEST_OUTPUT_ON_FAILURE=1 make test
