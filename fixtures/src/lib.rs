a6e4c2574883fc679dc70089db95e90c5c15eef8
Tomasz Kłak <tomasz.klak@nordsec.com>
2025-10-13 15:04:09 +0200

    Rename struct fields that use a name which is reserved on the c++ side
    Signed-off-by: Tomasz Kłak <tomasz.klak@nordsec.com>

--- fixtures/src/lib.rs
+++ fixtures/src/lib.rs
@@ -17,4 +17,5 @@ mod uniffi_fixtures {
     uniffi_cpp_custom_types_builtin::uniffi_reexport_scaffolding!();
     uniffi_enum_style_test::uniffi_reexport_scaffolding!();
     uniffi_empty_type::uniffi_reexport_scaffolding!();
+    uniffi_reserved_field_name::uniffi_reexport_scaffolding!();
 }
