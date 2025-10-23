348ca081f11f6cefe50de8b21594ab8f2c67b504
Tomasz Kłak <tomasz.klak@nordsec.com>
2025-10-23 10:09:36 +0200

    Expand the set of names for which `_` is appended to include few commonly used macros
    Signed-off-by: Tomasz Kłak <tomasz.klak@nordsec.com>

--- cpp-tests/tests/reserved_field_name/main.cpp
+++ cpp-tests/tests/reserved_field_name/main.cpp
@@ -3,8 +3,8 @@
 #include <reserved_field_name.hpp>
 
 int main() {
-    reserved_field_name::Foo foo = {42};
-    reserved_field_name::assert_this_is_42(foo);
+    reserved_field_name::Foo foo = {42, 42, 42};
+    reserved_field_name::assert_fields_are_42(foo);
 
     return 0;
 }
